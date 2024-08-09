611219a98d2e9c34eb58cbd3406a434ea2a8ab89
leerlandais <leerlandais@gmail.com>
2024-08-09 15:11:06 +0100

    privateView: added Admin Homepage, header and sidebar

---
+++ view/privateView/inc/privateHeader.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+
+<nav class="bg-blue-500 p-4 flex items-center justify-between">
+    <div>
+        <h1 class="text-white text-xl font-semibold">Code Snippets</h1>
+    </div>
+    <div class="flex items-center space-x-4">
+        <a href="?logout"><span class="text-white">Logout</span></a>
+        <i class="fas fa-user-circle text-white text-2xl"></i>
+    </div>
+</nav>
--- view/privateView/private.home.view.php
+++ view/privateView/private.home.view.php
@@ -2,7 +2,6 @@
 
 ?>
 
-
 <!doctype html>
 <html lang="en">
 <head>
@@ -12,19 +11,22 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <?php include "../view/allCdn.php"; ?>
     <title><?=$title?></title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
 </head>
-<body>
-<div class="container-fluid bg-green-300 h-screen text-center">
-<?php
-if (isset($systemMessage)) {
-    ?>
-    <h2 class="text-4xl text-red-700"><?=$systemMessage?></h2>
-    <?php
-}
-?>
-<h1>Private Good</h1>
-<a href="?logout">Logout</a>
-
+<body class="bg-gray-100">
+<?php include "inc/privateHeader.php"; ?>
+<div class="flex">
+    <?php include "inc/privateSideBar.php"; ?>
+    <main class="flex-1 p-6">
+        <?php if (isset($systemMessage)) echo '<h2 class="text-4xl text-red-700 text-center">'.$systemMessage.'</h2>'; ?>
+    </main>
 </div>
+
+
+
+<script src="scripts/sideBarScript.js">
+
+</script>
+
 </body>
 </html>
